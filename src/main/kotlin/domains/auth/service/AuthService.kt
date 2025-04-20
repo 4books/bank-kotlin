d83f4be5a085855448dc2d2c61809364377d4e5d
naegwon <joonwoo9192@gmail.com>
2025-04-20 21:17:24 +0900

    변경

--- src/main/kotlin/domains/auth/service/AuthService.kt
+++ src/main/kotlin/domains/auth/service/AuthService.kt
@@ -1,2 +1,27 @@
 package com.example.domains.auth.service
 
+import com.example.common.exception.CustomException
+import com.example.common.exception.ErrorCode
+import com.example.common.jwt.JwtProvider
+import com.example.interfaces.OAuthServiceInterface
+import org.springframework.stereotype.Service
+
+@Service
+class AuthService (
+    private val oAuthServices: Map<String, OAuthServiceInterface>,
+    private val jwtProvider: JwtProvider
+) {
+    fun handleAuth(state: String, code: String): String {
+        val provider = state.lowercase()
+        val callService = oAuthServices[provider] ?: throw CustomException(ErrorCode.PROVIDER_NOT_FOUND, provider)
+
+        val accessToken = callService.getToken(code)
+        val userInfo = callService.getUserInfo(accessToken.accessToken)
+        val token = jwtProvider.createToken(provider, userInfo.email, userInfo.name, userInfo.id)
+
+        //userinfo
+
+
+        return ""
+    }
+}
